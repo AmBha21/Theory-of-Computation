f2d1f29f92290c46bde0fe0f1eb71929ec9a7dd2
abhagavathi <amol-bhagavathi@uiowa.edu>
2023-12-01 15:56:53 -0600

    Moving Project from Local to GitHub 3 Third part of the final project, creating the method that visualizes the NFA created.

--- nfa_datatype.py
+++ nfa_datatype.py
@@ -1,3 +1,5 @@
+from graphviz import Digraph
+
 class StateNode:
     def __init__(self, state_id):
         self.state_id = state_id
@@ -33,6 +35,46 @@ class NFA:
 
     def add_epsilon_transition(self, from_state, to_state):
         self.states[from_state].add_epsilon_transition(to_state)
+
+    def to_dot(self):
+        dot_str = 'digraph NFA {\n'
+        dot_str += '    rankdir=LR;\n'
+        dot_str += '    node [shape = doublecircle]; ' + ' '.join(str(s) for s in self.accept_states) + ';\n'
+        dot_str += '    node [shape = circle];\n'
+
+        for state, node in self.states.items():
+            for symbol, destinations in node.transitions.items():
+                for dest in destinations:
+                    dot_str += f'    {state} -> {dest} [label="{symbol}"];\n'
+            for dest in node.epsilon_transitions:
+                dot_str += f'    {state} -> {dest} [label="ε"];\n'
+
+        dot_str += '}'
+        return dot_str
+
+    def generate_graphviz_file(self, filename):
+        with open(filename, 'w') as file:
+            file.write(self.to_dot())
+        print(f"GraphViz file generated: {filename}")
+
+    def render_graph(self, filename='nfa'):
+        dot = Digraph()
+
+        for state in self.states:
+            if state in self.accept_states:
+                dot.node(str(state), shape='doublecircle')
+            else:
+                dot.node(str(state), shape='circle')
+
+        for state, node in self.states.items():
+            for symbol, destinations in node.transitions.items():
+                for dest in destinations:
+                    dot.edge(str(state), str(dest), label=symbol)
+            for dest in node.epsilon_transitions:
+                dot.edge(str(state), str(dest), label='ε')
+
+        dot.render(filename, format='png', cleanup=True)
+        print(f"Graph rendered and saved as {filename}.png")
 
     # TODO, maybe additional methods for simulating the NFA, checking input strings, etc.
 
@@ -70,3 +112,16 @@ class NFA:
 
 # # Run the test
 # test_nfa_structure()
+
+nfa = NFA()
+nfa.add_state(1)
+nfa.add_state(2)
+nfa.add_state(3)
+nfa.set_start_state(1)
+nfa.add_accept_state(3)
+nfa.add_transition(1, 'a', 2)
+nfa.add_transition(2, 'b', 3)
+nfa.add_epsilon_transition(1, 3)
+
+nfa.generate_graphviz_file('nfa.dot')
+
